446d7a25480c8fceae533b467b32a416cfa11b86
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2017-12-16 08:33:29 -0500

    Initial Commit

---
+++ OneDollarGestureRecognizer/OneDollarGestureRecognizer.h
@@ -0,0 +1,19 @@
+//
+//  OneDollarGestureRecognizer.h
+//  OneDollarGestureRecognizer
+//
+//  Created by Daniel Cardona on 12/16/17.
+//  Copyright © 2017 Daniel Cardona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneDollarGestureRecognizer.
+FOUNDATION_EXPORT double OneDollarGestureRecognizerVersionNumber;
+
+//! Project version string for OneDollarGestureRecognizer.
+FOUNDATION_EXPORT const unsigned char OneDollarGestureRecognizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneDollarGestureRecognizer/PublicHeader.h>
+
+
